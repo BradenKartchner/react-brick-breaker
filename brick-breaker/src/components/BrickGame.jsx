60de6196c165f2ccead310ec1dfa18928c3342aa
Braden Kartchner <62575814+BradenKartchner@users.noreply.github.com>
2024-01-17 16:53:58 -0700

    finally got the ball to move and the main gameplay loop to work

--- brick-breaker/src/components/BrickGame.jsx
+++ brick-breaker/src/components/BrickGame.jsx
@@ -3,6 +3,9 @@ import { useState, useEffect, useRef } from "react";
 
 function BrickGame() {
     // Global vars that do NOT need state (don't re-render canvas when changed)
+    var myInterval, ctx, width, height;
+    var ballColor = "black";
+    var ballRadius = 10;
     // Global vars that DO need state (re-render canvas when changed)
     const [x, setX] = useState(200);
     const [y, setY] = useState(150);
@@ -14,16 +17,51 @@ function BrickGame() {
     // get reference to Canvas
     const canvasRef = useRef(null);
 
+    // function to draw the ball
+    function circle(a, b, c) {
+        ctx.beginPath();
+        ctx.arc(a, b, c, 0, Math.PI * 2, true);
+        ctx.closePath();
+        ctx.fill();
+    }
+
     function draw() {
-        console.log("ran draw function");
+        // clear everything from the canvas
+        // draw the ball
+        ctx.fillStyle = ballColor;
+        circle(x, y, ballRadius);
+
+        // reverse x if the ball hits the side of the canvas
+        if (x + dx > width || x + dx < 0) {
+            setDX(-dx);
+        }
+
+        // handle changes in y
+        if (y + dy < 0) {
+            setDY(-dy);
+        } else if (y + dy > height) {
+            setDY(-dy);
+        }
+
+        // change x and y values
+        setY(y + dy);
+        setX(x + dx);
     }
 
     // main loop: useEffect()
     useEffect(() => {
         var myCanvas = canvasRef.current;
-        console.log(myCanvas);
-        draw();
-    }, [canvasRef, x, y, dx, dy]);
+        ctx = myCanvas.getContext("2d");
+        width = myCanvas.width;
+        height = myCanvas.height;
+        //draw();
+        // Game loop: run draw on an interval
+        myInterval = setInterval(draw, 100);
+
+        //
+        return () => clearInterval(myInterval);
+    });
+    //[canvasRef, x, y, dx, dy]
 
     return (
         <>
